4c06c83c9851181515790bcddd11c855702562de
MUKILTEO\1554910 <1554910@mukilteo.wednet.edu>
2025-03-21 13:08:28 -0700

    no message

--- 3D Template/Assets/Scripts/WallRunning.cs
+++ 3D Template/Assets/Scripts/WallRunning.cs
@@ -2,13 +2,29 @@ using UnityEngine;
 
 public class WallRunning : MonoBehaviour
 {
-    void Start()
-    {
-        
-    }
+    [Header("Wallrunning")]
+    public LayerMask whatIsWall;
+    public LayerMask whatIsGround;
+    public float wallRunForce;
+    public float maxwallRunTime;
+    private float wallRunTimer;
 
-    void Update()
-    {
-        
-    }
+    [Header("Input")]
+    private float horizontalInput;
+    private float verticalInput;
+
+    [Header("Detection")]
+    public float wallCheckDistance;
+    public float minJumpHeight;
+    private RaycastHit leftwallhut;
+    private RaycastHit rightwallhut;
+    private bool wallLeft;
+    private bool wallRight;
+
+    [Header("References")]
+    public Transform orientation;
+    private PlayerMovement pm;
+    private Rigidbody rb;
+
+
 }
